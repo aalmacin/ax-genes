f7b1b547226ca098150828b31598da796af493f9
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-11-27 02:37:04 -0500

    Catch issues

--- index.ts
+++ index.ts
@@ -162,6 +162,6 @@ async function getAxiesFromMarketPlace() {
         }
         console.log(dataFrom, returnData)
     })
-    await Promise.all(axiePromises)
+    await Promise.all(axiePromises.map(p => p.catch(e => console.log(e))))
     redisClient.quit();
 })();
