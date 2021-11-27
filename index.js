fcb94d3cf7952349665d85024808fe22957e6342
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-11-26 22:24:43 -0500

    Add google datastore

--- index.js
+++ index.js
@@ -2,6 +2,97 @@ const { AxieGene } = require("agp-npm/dist/axie-gene");
 const fetch = require("node-fetch");
 
 (async () => {
+    const marketResponse = await fetch("https://graphql-gateway.axieinfinity.com/graphql", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            query: `query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String, $filterStuckAuctions: Boolean) {
+                axies(
+                  auctionType: $auctionType
+                  criteria: $criteria
+                  from: $from
+                  sort: $sort
+                  size: $size
+                  owner: $owner
+                  filterStuckAuctions: $filterStuckAuctions
+                ) {
+                  total
+                  results {
+                    ...AxieBrief
+                    __typename
+                  }
+                  __typename
+                }
+              }
+              
+              fragment AxieBrief on Axie {
+                id
+                name
+                stage
+                class
+                breedCount
+                image
+                title
+                battleInfo {
+                  banned
+                  __typename
+                }
+                auction {
+                  currentPrice
+                  currentPriceUSD
+                  __typename
+                }
+                parts {
+                  id
+                  name
+                  class
+                  type
+                  specialGenes
+                  __typename
+                }
+                __typename
+              }
+            `,
+            variables: {
+                "from": 0,
+                "size": 10000000,
+                "sort": "PriceAsc",
+                "auctionType": "Sale",
+                "owner": null,
+                "criteria": {
+                  "region": null,
+                  "parts": [
+                    "tail-yam",
+                    "tail-carrot",
+                    "back-pumpkin",
+                    "mouth-serious",
+                    "horn-cactus"
+                  ],
+                  "bodyShapes": null,
+                  "classes": [
+                    "Plant"
+                  ],
+                  "stages": null,
+                  "numMystic": null,
+                  "pureness": null,
+                  "title": null,
+                  "breedable": null,
+                  "breedCount": null,
+                  "hp": [],
+                  "skill": [],
+                  "speed": [],
+                  "morale": []
+                },
+                "filterStuckAuctions": true
+              } 
+        })
+    });
+    const res = await marketResponse.json();
+    const axies = res.data.axies.results;
+    console.log(axies);
+
     const response =  await fetch("https://axieinfinity.com/graphql-server-v2/graphql", {
         method: "POST",
         headers: {
